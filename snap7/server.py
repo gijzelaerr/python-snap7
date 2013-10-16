2ce88e6fe1de6c9fb04aef8e22a1ed44a67beb9c
Gijs Molenaar <gijs@pythonic.nl>
2013-10-16 19:34:40 +0200

    implement all server functions

--- snap7/server.py
+++ snap7/server.py
@@ -1,5 +1,7 @@
 import ctypes
 import logging
+import re
+from decorator import decorator
 from loadlib import clib
 from snap7.types import S7Object, time_t, longword, word
 from snap7.error import error_parse
@@ -13,6 +15,23 @@ srvAreaMK = 2
 srvAreaCT = 3
 srvAreaTM = 4
 srvAreaDB = 5
+
+ipv4 = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
+
+
+def check_error(code):
+    errors = error_parse(code, client=False)
+    if errors:
+        for error in errors:
+            logging.error(error)
+        raise Exception(", ".join(errors))
+
+
+def error_wrap(func):
+    def f(func, *args, **kw):
+        code = func(*args, **kw)
+        check_error(code)
+    return decorator(f, func)
 
 
 server_statuses = {
@@ -28,7 +47,7 @@ cpu_statuses = {
 }
 
 
-class PSrvEvent(ctypes.Structure):
+class SrvEvent(ctypes.Structure):
     _fields_ = [
         ('EvtTime', time_t),
         ('EvtSender', ctypes.c_int),
@@ -41,25 +60,20 @@ class PSrvEvent(ctypes.Structure):
     ]
 
     def __str__(self):
-        return "time: %s sender: %s code: %s retcode: %s param1: %s param2:" \
-               " %s param3: %s param4: %s " % (self.EvtTime, self.EvtSender,
-                                               self.EvtCode, self.EvtRetCode,
-                                               self.EvtParam1, self.EvtParam2,
-                                               self.EvtParam3, self.EvtParam4)
+        return "<event time: %s sender: %s code: %s retcode: %s param1: " \
+               "%s param2:%s param3: %s param4: %s>" % (
+                                                        self.EvtTime,
+                                                        self.EvtSender,
+                                                        self.EvtCode,
+                                                        self.EvtRetCode,
+                                                        self.EvtParam1,
+                                                        self.EvtParam2,
+                                                        self.EvtParam3,
+                                                        self.EvtParam4)
 
 
 CALLBACK = ctypes.CFUNCTYPE(ctypes.c_int, ctypes.c_void_p,
-                            ctypes.POINTER(PSrvEvent), ctypes.c_uint)
-
-
-def error_wrap(code):
-    errors = error_parse(code, client=False)
-    if errors:
-        for error in errors:
-            logging.error(error)
-        raise Exception(", ".join(errors))
-
-
+                            ctypes.POINTER(SrvEvent), ctypes.c_uint)
 
 
 def error_text(error):
@@ -85,8 +99,8 @@ def event_text(event):
     len_ = 1024
     text_type = ctypes.c_char * len_
     text = text_type()
-    error_wrap(clib.Srv_EventText(ctypes.byref(event), ctypes.byref(text),
-                                  len_))
+    error = clib.Srv_EventText(ctypes.byref(event), ctypes.byref(text), len_)
+    check_error(error)
     return text.value
 
 
@@ -94,16 +108,19 @@ class Server(object):
     def __init__(self):
         logger.info("creating server")
         self.pointer = S7Object(clib.Srv_Create())
+        self._set_log_callback()
 
+    @error_wrap
     def register_area(self, area_code, index, userdata):
         """Shares a memory area with the server. That memory block will be
         visible by the clients.
         """
         logger.info("registering area")
         size = ctypes.sizeof(userdata)
-        error_wrap(clib.Srv_RegisterArea(self.pointer, area_code, index,
-                                         ctypes.byref(userdata), size))
+        return clib.Srv_RegisterArea(self.pointer, area_code, index,
+                                     ctypes.byref(userdata), size)
 
+    @error_wrap
     def set_events_callback(self, call_back):
         """Sets the user callback that the Server object has to call when an
         event is created.
@@ -117,26 +134,37 @@ class Server(object):
             :param size:
             :returns: should return an int
             """
-            # TODO: call the actual callback function
+            # TODO: call the actual callback function. Somehow we can't access
+            # objects in the scope of this object...
             logger.info(event_text(pevent.contents))
             return 0
-        error_wrap(clib.Srv_SetEventsCallback(self.pointer,
-                                              CALLBACK(wrap_callback)))
 
+        return clib.Srv_SetEventsCallback(self.pointer, CALLBACK(wrap_callback))
 
+    @error_wrap
+    def _set_log_callback(self):
+        """Sets a callback that logs the events
+        """
+        logger.debug("setting up event logger")
+        def wrap_callback(usrptr, pevent, size):
+            logger.info(event_text(pevent.contents))
+            return 0
+        return clib.Srv_SetEventsCallback(self.pointer, CALLBACK(wrap_callback))
+
+    @error_wrap
     def start(self):
-        logger.info("starting server")
-        error_wrap(clib.Srv_Start(self.pointer))
+        logger.info("starting server on 0.0.0.0:102")
+        return clib.Srv_Start(self.pointer)
 
-
+    @error_wrap
     def stop(self):
         logger.info("stopping server")
-        error_wrap(clib.Srv_Stop(self.pointer))
+        return clib.Srv_Stop(self.pointer)
 
+    @error_wrap
     def destroy(self):
         logger.info("destroying server")
         return clib.Srv_Destroy(ctypes.byref(self.pointer))
-
 
     def get_status(self):
         """Reads the server status, the Virtual CPU status and the number of
@@ -144,34 +172,107 @@ class Server(object):
 
         :returns: server status, cpu status, client count
         """
-        logger.info("get server status")
+        logger.debug("get server status")
         server_status = ctypes.c_int()
         cpu_status = ctypes.c_int()
         clients_count = ctypes.c_int()
-        error_wrap(clib.Srv_GetStatus(self.pointer, ctypes.byref(server_status),
+        error = (clib.Srv_GetStatus(self.pointer, ctypes.byref(server_status),
                                     ctypes.byref(cpu_status),
                                     ctypes.byref(clients_count)))
+        check_error(error)
+        logger.debug("status server %s cpu %s clients %s" % (server_status.value,
+                     cpu_status.value, clients_count.value))
         return server_statuses[server_status.value],\
                cpu_statuses[cpu_status.value],\
                clients_count.value
 
+    @error_wrap
+    def unregister_area(self, area_code, index):
+        """'Unshares' a memory area previously shared with Srv_RegisterArea().
+        That memory block will be no longer visible by the clients.
+        """
+        return clib.Srv_UnregisterArea(self.pointer, area_code, index)
 
+    @error_wrap
+    def unlock_area(self,  code, index):
+        """Unlocks a previously locked shared memory area.
+        """
+        logging.debug("unlocking area code %s index %s" % (code, index))
+        return clib.Srv_UnlockArea(self.pointer, code, index)
 
+    @error_wrap
+    def lock_area(self,  code, index):
+        """Locks a shared memory area.
+        """
+        logging.debug("locking area code %s index %s" % (code, index))
+        return clib.Srv_UnlockArea(self.pointer, code, index)
 
+    @error_wrap
+    def start_to(self, ip):
+        assert re.match(ipv4, ip), '%s is invalid ipv4' % ip
+        logger.info("starting server to %s:102" % ip)
+        return clib.Srv_Start(self.pointer, ip)
 
-"""
-Srv_ClearEvents
-Srv_EventText
-Srv_GetMask
-Srv_GetParam
-Srv_LockArea
-Srv_PickEvent
-Srv_RegisterArea
-Srv_SetCpuStatus
-Srv_SetMask
-Srv_SetParam
-Srv_StartTo
-Srv_UnlockArea
-Srv_UnregisterArea
-"""
+    @error_wrap
+    def set_param(self, number, value):
+        """Sets an internal Server object parameter.
+        """
+        logger.debug("setting param number %s to %s" % (number, value))
+        return clib.Srv_SetParam(self.pointer, number,
+                                 ctypes.byref(ctypes.c_int(value)))
 
+    @error_wrap
+    def set_mask(self, kind, mask):
+        """Writes the specified filter mask.
+        """
+        logger.debug("setting mask kind %s to %s" % (kind, mask))
+        return clib.Srv_SetMask(self.pointer, kind, mask)
+
+    @error_wrap
+    def set_cpu_status(self, status):
+        """Sets the Virtual CPU status.
+        """
+        assert status in cpu_statuses, 'unknown cpu state %s' % status
+        logger.debug("setting cpu status to %s" % status)
+        return clib.Srv_SetCpuStatus(self.pointer, status)
+
+    def pick_event(self):
+        """Extracts an event (if available) from the Events queue.
+        """
+        logger.debug("checking event queue")
+        event = SrvEvent()
+        ready = ctypes.c_int32()
+        code = clib.Srv_PickEvent(self.pointer, ctypes.byref(event),
+                              ctypes.byref(ready))
+        check_error(code)
+        if ready:
+            logger.debug("no events ready")
+            return event
+        logger.debug("one event ready: %s" % event)
+
+    def get_param(self, number):
+        """Reads an internal Server object parameter.
+
+        int Srv_GetParam(S7Object Server, int ParamNumber, void *pValue);
+        """
+        logger.debug("retreiving param number %s" % number)
+        value = ctypes.c_int()
+        code = clib.Srv_GetParam(self.pointer, number, ctypes.byref(value))
+        check_error(code)
+        return value.value
+
+    def get_mask(self, kind):
+        """Reads the specified filter mask.
+        """
+        logger.debug("retrieving mask kind %s" % kind)
+        mask = longword()
+        code = clib.Srv_GetMask(self.pointer, kind, ctypes.byref(mask))
+        check_error(code)
+        return mask
+
+    @error_wrap
+    def clear_events(self):
+        """Empties the Event queue.
+        """
+        logger.debug("clearing event queue")
+        return clib.Srv_ClearEvents(self.pointer)
--- snap7/types.py
+++ snap7/types.py
@@ -6,3 +6,24 @@ buffer_type = c_ubyte * buffer_size
 time_t = c_uint64  # TODO: check if this is valid for all platforms
 word = c_uint16
 longword = c_uint32
+
+# // PARAMS LIST
+LocalPort       = 1
+RemotePort      = 2
+PingTimeout     = 3
+SendTimeout     = 4
+RecvTimeout     = 5
+WorkInterval    = 6
+SrcRef          = 7
+DstRef          = 8
+SrcTSap         = 9
+PDURequest      = 10
+MaxClients      = 11
+BSendTimeout    = 12
+BRecvTimeout    = 13
+RecoveryTime    = 14
+KeepAliveTime   = 15
+
+# mask types
+mkEvent = 0
+mkLog = 1
