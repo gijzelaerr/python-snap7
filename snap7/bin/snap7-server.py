773e3c3b4d13cff68df55e31e487a41119a9549b
Gijs Molenaar <gijs@pythonic.nl>
2013-10-19 16:08:49 +0200

    add server script

---
+++ snap7/bin/snap7-server.py
@@ -0,0 +1,23 @@
+import time
+import logging
+import snap7
+
+logging.basicConfig()
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def server():
+    server = snap7.server.Server()
+    server.start()
+    while True:
+        logger.info("server: %s cpu: %s users: %s" % server.get_status())
+        while True:
+            event = server.pick_event()
+            if event:
+                logger.info(snap7.server.event_text(event))
+            else:
+                break
+        time.sleep(1)
+
+if __name__ == '__main__':
+    server()
