950398d204ab7c4d0ffd77c95032cb6a4da81f24
Gijs Molenaar <gijs@pythonic.nl>
2013-11-09 16:35:36 +0100

    fix pypi entry

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
+import os
 from distutils.core import setup
 
-setup(name='snap7',
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name='python-snap7',
       version='0.1',
       description='Python wrapper for the snap7 library',
       author='Gijs Molenaar',
@@ -8,7 +12,7 @@ setup(name='snap7',
       url='https://github.com/gijzelaerr/python-snap7',
       packages=['snap7'],
       license='MIT licence, see LICENCE',
-      long_description=open('README.rst').read(),
+      long_description=read('README.rst'),
       scripts=['snap7/bin/snap7-server.py'],
 )
 
