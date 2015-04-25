3cd8d9a29ef6cb0fc300bc8891fe70c05bd8532e
Nathan Rees <Nextabyte@gmail.com>
2015-04-24 22:53:04 -0700

    added Connect() for Cli_Connect for s7-200/logo plcs

--- snap7/client.py
+++ snap7/client.py
@@ -114,6 +114,13 @@ class Client(object):
             self.pointer, c_char_p(six.b(address)),
             c_int(rack), c_int(slot))
 
+    def connect(self):
+		"""To connect to these PLC you need to call set_connection_params() and then 
+		connect().  S7-200/Logo PLCs requiring TSAPs
+		"""
+		logger.debug("connecting using set_connection_params")
+		return self.library.Cli_Connect(self.pointer)
+
     def db_read(self, db_number, start, size):
         """This is a lean function of Cli_ReadArea() to read PLC DB.
 
@@ -523,7 +530,7 @@ class Client(object):
         type_ = param_types[number]
         return self.library.Cli_SetParam(self.pointer, number,
                                          byref(type_(value)))
-
+	
     def get_param(self, number):
         """Reads an internal Client object parameter.
         """
@@ -534,3 +541,6 @@ class Client(object):
                                          byref(value))
         check_error(code)
         return value.value
+
+
+		
