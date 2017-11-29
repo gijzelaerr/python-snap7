7543fd93e975f05e4dcc617ef1d7683f89f722d2
openarchitect <martin.zimmermann@gmx.de>
2017-11-29 07:10:34 +0100

    Update __init__.py (#89)
    Add logo

--- snap7/__init__.py
+++ snap7/__init__.py
@@ -9,6 +9,8 @@ import snap7.error as error
 import snap7.snap7types as types
 import snap7.common as common
 import snap7.util as util
+import snap7.logo as logo
+
 
 try:
     __version__ = pkg_resources.require("snap7")[0].version
