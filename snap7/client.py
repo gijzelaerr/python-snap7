99f156516433c36e972e90839da4acc0b7cd8332
Fabian Beitler <f.beitler@konzeptpark.de>
2020-07-03 12:04:28 +0200

    Remove unecessarry return

--- snap7/client.py
+++ snap7/client.py
@@ -652,7 +652,6 @@ class ClientAsync(Client):
         else:
             self.as_check = mode
             logger.debug("Async check mode changed to %s", mode)
-        return
 
     async def async_wait_loop(self):
         """
