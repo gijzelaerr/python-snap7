8fc247d3d20d331f766c5d481d33eade25693afc
Fabian Beitler <f.beitler@konzeptpark.de>
2020-07-22 12:09:46 +0200

    replace echo with printf

--- run_tests.sh
+++ run_tests.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 export PYTHONPATH=.
-echo -e "Running test_partner.py ----------------------------------------------\n\n"
+printf "Running test_partner.py ----------------------------------------------\n\n"
 sudo nosetests test/test_partner.py
-echo -e "Running test_server.py  ----------------------------------------------\n\n"
+printf "Running test_server.py  ----------------------------------------------\n\n"
 nosetests test/test_server.py
-echo -e "Running test_client.py  ----------------------------------------------\n\n"
+printf "Running test_client.py  ----------------------------------------------\n\n"
 nosetests test/test_client.py
-echo -e "Running test_client_async.py  ----------------------------------------------\n\n"
+printf "Running test_client_async.py  ----------------------------------------------\n\n"
 nosetests test/test_client_async.py
-echo -e "Running test_util.py    ----------------------------------------------\n\n"
+printf "Running test_util.py    ----------------------------------------------\n\n"
 nosetests test/test_util.py
 
