4535a590949874e868f1c992d8b1b7b43704e1c9
Fabian Beitler <f.beitler@konzeptpark.de>
2020-07-28 09:06:44 +0200

    add sudos to tests

--- .travis/script.sh
+++ .travis/script.sh
@@ -9,9 +9,8 @@ else
 fi
 
 PYTHONPATH=.
-python3 -c 'print("öööööööööööööööööööööööööööö");help("modules");print("öööööööööööööööööööööööööööö")'
-${NOSETESTS} --with-coverage test/test_server.py
-${NOSETESTS} --with-coverage test/test_client.py
-${NOSETESTS} --with-coverage test/test_client_async.py
-${NOSETESTS} --with-coverage test/test_util.py
+sudo ${NOSETESTS} --with-coverage test/test_server.py
+sudo ${NOSETESTS} --with-coverage test/test_client.py
+sudo ${NOSETESTS} --with-coverage test/test_client_async.py
+sudo ${NOSETESTS} --with-coverage test/test_util.py
 sudo ${NOSETESTS} --with-coverage test/test_partner.py
