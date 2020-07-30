5030ccb49e3b3eaae7042c8c65dc7f456995113b
Fabian Beitler <f.beitler@konzeptpark.de>
2020-07-30 11:33:44 +0200

    Add client_async to init

--- snap7/__init__.py
+++ snap7/__init__.py
@@ -4,11 +4,12 @@ The Snap7 Python library.
 import pkg_resources
 
 import snap7.client as client
+import snap7.client_async as client_async
 import snap7.common as common
 import snap7.error as error
 import snap7.logo as logo
 import snap7.server as server
-import snap7.types as types
+import snap7.snap7types as types
 import snap7.util as util
 
 try:
