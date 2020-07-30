644f73070ccf32353d5f5b39e63cbaff24af604d
Fabian Beitler <f.beitler@konzeptpark.de>
2020-07-30 13:35:24 +0200

    Add test_client_async to tests

--- run_tests.sh
+++ run_tests.sh
@@ -7,5 +7,6 @@ export PYTHONPATH=.
 sudo venv/bin/nosetests test/test_partner.py
 nosetests test/test_server.py
 nosetests test/test_client.py
+nosetests test/test_client_async.py
 nosetests test/test_util.py
 
