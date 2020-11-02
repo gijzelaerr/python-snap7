cb4e5a13df56bb7e41380dd618f86c39a55d0baa
Fabian Beitler <f.beitler@konzeptpark.de>
2020-11-02 12:56:58 +0100

    Revert "remove partner, since other stuff may happen there"
    This reverts commit c236c7f4ede735718b2cbfab1e515b0acbfc86a8.
    Revert "Revert test to normal"
    
    This reverts commit d6564c3fbd01f7e5ad53199eb7f37d9fc598b092.
    Revert "Add multiple test changes to tackle windows test"
    
    This reverts commit 1da71af2277983b79e55d6f4f49c401bdf44d5b3.

--- test/test_client_async.py
+++ test/test_client_async.py
@@ -1,14 +1,45 @@
 import asyncio
 import ctypes
 import logging
+import time
 import pytest
+from multiprocessing import Process
+from os import kill
+
+import snap7
+from snap7.server import mainloop
 
 logging.basicConfig(level=logging.WARNING)
 
 pytestmark = pytest.mark.asyncio
 
 
-async def test_wait_loop(testasyncclient):
+ip = '127.0.0.1'
+tcpport = 1102
+db_number = 1
+rack = 1
+slot = 1
+
+
+@pytest.fixture
+def testserver():
+    process = Process(target=mainloop)
+    process.start()
+    time.sleep(2)  # wait for server to start
+    yield process
+    kill(process.pid, 1)
+
+
+@pytest.fixture
+def testclient():
+    client_async = snap7.client_async.ClientAsync()
+    client_async.connect(ip, rack, slot, tcpport)
+    yield client_async
+    client_async.disconnect()
+    client_async.destroy()
+
+
+async def test_wait_loop(testclient):
     check_status = ctypes.c_int(-1)
     pending_checked = False
     # preparing Server values
@@ -16,10 +47,10 @@ async def test_wait_loop(testasyncclient):
     start = 0
     db = 1
     data = bytearray(40)
-    testasyncclient.db_write(db_number=db, start=start, data=data)
+    testclient.db_write(db_number=db, start=start, data=data)
     # Execute test
-    p_data = testasyncclient.as_db_read(db, start, size)
-    await asyncio.wait_for(testasyncclient.wait_loop(check_status), 10)
+    p_data = testclient.as_db_read(db, start, size)
+    await asyncio.wait_for(testclient.wait_loop(check_status), 10)
     data_result = bytearray(p_data)
     if not data == data_result:
         logging.warning("Test result is not as expected")
--- test/test_partner.py
+++ test/test_partner.py
@@ -5,134 +5,112 @@ from unittest import mock
 import snap7.partner
 from snap7.exceptions import Snap7Exception
 
-import pytest
-
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.WARNING)
 
 
-@pytest.mark.skip("skip as stuff, since it may be broken at the moment")
-def test_as_b_send(testpartner):
-    testpartner.partner.as_b_send()
+class TestPartner(unittest.TestCase):
+    def setUp(self):
+        self.partner = snap7.partner.Partner()
+        self.partner.start()
 
+    def test_as_b_send(self):
+        self.partner.as_b_send()
 
-@pytest.mark.skip("we don't recv something yet")
-def test_b_recv(testpartner):
-    testpartner.partner.b_recv()
+    @unittest.skip("we don't recv something yet")
+    def test_b_recv(self):
+        self.partner.b_recv()
 
+    def test_b_send(self):
+        self.partner.b_send()
 
-def test_b_send(testpartner):
-    testpartner.partner.b_send()
+    def test_check_as_b_recv_completion(self):
+        self.partner.check_as_b_recv_completion()
 
+    def test_check_as_b_send_completion(self):
+        self.partner.check_as_b_send_completion()
 
-@pytest.mark.skip("skip as stuff, since it may be broken at the moment")
-def test_check_as_b_recv_completion(testpartner):
-    testpartner.partner.check_as_b_recv_completion()
+    def test_create(self):
+        self.partner.create()
 
+    def test_destroy(self):
+        self.partner.destroy()
 
-@pytest.mark.skip("skip as stuff, since it may be broken at the moment")
-def test_check_as_b_send_completion(testpartner):
-    testpartner.partner.check_as_b_send_completion()
+    def test_error_text(self):
+        snap7.common.error_text(0, context="partner")
 
+    def test_get_last_error(self):
+        self.partner.get_last_error()
 
-def test_create(testpartner):
-    testpartner.partner.create()
+    def test_get_param(self):
+        expected = (
+            (snap7.types.LocalPort, 0),
+            (snap7.types.RemotePort, 102),
+            (snap7.types.PingTimeout, 750),
+            (snap7.types.SendTimeout, 10),
+            (snap7.types.RecvTimeout, 3000),
+            (snap7.types.SrcRef, 256),
+            (snap7.types.DstRef, 0),
+            (snap7.types.SrcTSap, 0),
+            (snap7.types.PDURequest, 480),
+            (snap7.types.WorkInterval, 100),
+            (snap7.types.BSendTimeout, 3000),
+            (snap7.types.BRecvTimeout, 3000),
+            (snap7.types.RecoveryTime, 500),
+            (snap7.types.KeepAliveTime, 5000),
+        )
+        for param, value in expected:
+            self.assertEqual(self.partner.get_param(param), value)
 
+        self.assertRaises(Exception, self.partner.get_param,
+                          snap7.types.MaxClients)
 
-def test_destroy(testpartner):
-    testpartner.partner.destroy()
+    def test_get_stats(self):
+        self.partner.get_stats()
 
+    def test_get_status(self):
+        self.partner.get_status()
 
-def test_error_text(testpartner):
-    snap7.common.error_text(0, context="partner")
+    def test_get_times(self):
+        self.partner.get_times()
 
+    def test_set_param(self):
+        values = (
+            (snap7.types.PingTimeout, 800),
+            (snap7.types.SendTimeout, 15),
+            (snap7.types.RecvTimeout, 3500),
+            (snap7.types.WorkInterval, 50),
+            (snap7.types.SrcRef, 128),
+            (snap7.types.DstRef, 128),
+            (snap7.types.SrcTSap, 128),
+            (snap7.types.PDURequest, 470),
+            (snap7.types.BSendTimeout, 2000),
+            (snap7.types.BRecvTimeout, 2000),
+            (snap7.types.RecoveryTime, 400),
+            (snap7.types.KeepAliveTime, 4000),
+        )
+        for param, value in values:
+            self.partner.set_param(param, value)
 
-def test_get_last_error(testpartner):
-    testpartner.partner.get_last_error()
+        self.assertRaises(Exception, self.partner.set_param,
+                          snap7.types.RemotePort, 1)
 
+    def test_set_recv_callback(self):
+        self.partner.set_recv_callback()
 
-def test_get_param(testpartner):
-    expected = (
-        (snap7.types.LocalPort, 0),
-        (snap7.types.RemotePort, 102),
-        (snap7.types.PingTimeout, 750),
-        (snap7.types.SendTimeout, 10),
-        (snap7.types.RecvTimeout, 3000),
-        (snap7.types.SrcRef, 256),
-        (snap7.types.DstRef, 0),
-        (snap7.types.SrcTSap, 0),
-        (snap7.types.PDURequest, 480),
-        (snap7.types.WorkInterval, 100),
-        (snap7.types.BSendTimeout, 3000),
-        (snap7.types.BRecvTimeout, 3000),
-        (snap7.types.RecoveryTime, 500),
-        (snap7.types.KeepAliveTime, 5000),
-    )
-    for param, value in expected:
-        testpartner.assertEqual(testpartner.partner.get_param(param), value)
+    def test_set_send_callback(self):
+        self.partner.set_send_callback()
 
-    testpartner.assertRaises(Exception, testpartner.partner.get_param,
-                             snap7.types.MaxClients)
+    def test_start(self):
+        self.partner.start()
 
+    def test_start_to(self):
+        self.partner.start_to('0.0.0.0', '0.0.0.0', 0, 0)
 
-def test_get_stats(testpartner):
-    testpartner.partner.get_stats()
+    def test_stop(self):
+        self.partner.stop()
 
-
-def test_get_status(testpartner):
-    testpartner.partner.get_status()
-
-
-def test_get_times(testpartner):
-    testpartner.partner.get_times()
-
-
-def test_set_param(testpartner):
-    values = (
-        (snap7.types.PingTimeout, 800),
-        (snap7.types.SendTimeout, 15),
-        (snap7.types.RecvTimeout, 3500),
-        (snap7.types.WorkInterval, 50),
-        (snap7.types.SrcRef, 128),
-        (snap7.types.DstRef, 128),
-        (snap7.types.SrcTSap, 128),
-        (snap7.types.PDURequest, 470),
-        (snap7.types.BSendTimeout, 2000),
-        (snap7.types.BRecvTimeout, 2000),
-        (snap7.types.RecoveryTime, 400),
-        (snap7.types.KeepAliveTime, 4000),
-    )
-    for param, value in values:
-        testpartner.partner.set_param(param, value)
-
-    testpartner.assertRaises(Exception, testpartner.partner.set_param,
-                             snap7.types.RemotePort, 1)
-
-
-def test_set_recv_callback(testpartner):
-    testpartner.partner.set_recv_callback()
-
-
-def test_set_send_callback(testpartner):
-    testpartner.partner.set_send_callback()
-
-
-@pytest.mark.skip("Test has to be skipped, since always a partner is started at the beginning")
-def test_start(testpartner):
-    testpartner.partner.start()
-
-
-def test_start_to(testpartner):
-    testpartner.partner.start_to('0.0.0.0', '0.0.0.0', 0, 0)
-
-
-@pytest.mark.skip("Test has to be skipped, since always a partner is stopped at the end")
-def test_stop(testpartner):
-    testpartner.partner.stop()
-
-
-@pytest.mark.skip("skip as stuff, since it may be broken at the moment")
-def test_wait_as_b_send_completion(testpartner):
-    testpartner.assertRaises(Snap7Exception, testpartner.partner.wait_as_b_send_completion)
+    def test_wait_as_b_send_completion(self):
+        self.assertRaises(Snap7Exception, self.partner.wait_as_b_send_completion)
 
 
 class TestLibraryIntegration(unittest.TestCase):
