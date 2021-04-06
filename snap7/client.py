800356e38f70f87299fa8afd321b8ade45ce4210
lautarodapin <lautarodapin@gmail.com>
2021-04-06 17:04:34 -0300

    fix example in `Client`

--- snap7/client.py
+++ snap7/client.py
@@ -32,18 +32,19 @@ class Client:
     """
     A snap7 client
 
-    >>> import snap7
-    >>> client = snap7.client.Client()
-    >>> client.connect("127.0.0.1", 0, 0, 1012)
-    >>> client.get_connected()
-    True
-    >>> data = client.db_read(1, 0, 4)
-    >>> data
-    bytearray(b"\x00\x00\x00\x00")
-    >>> data[3] = 0b00000001
-    >>> data
-    bytearray(b'\x00\x00\x00\x01')
-    >>> data.db_write(1, 0, data)
+    Examples:
+        >>> import snap7
+        >>> client = snap7.client.Client()
+        >>> client.connect("127.0.0.1", 0, 0, 1012)
+        >>> client.get_connected()
+        True
+        >>> data = client.db_read(1, 0, 4)
+        >>> data
+        bytearray(b"\x00\x00\x00\x00")
+        >>> data[3] = 0b00000001
+        >>> data
+        bytearray(b'\x00\x00\x00\x01')
+        >>> data.db_write(1, 0, data)
     """
 
     def __init__(self, lib_location: Optional[str] = None):
