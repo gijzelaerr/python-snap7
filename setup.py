ff497431137faceb0dcbda266f64c278ea37749a
Gijs Molenaar <gijs@pythonic.nl>
2021-07-07 10:49:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 
 from setuptools import setup, find_packages, Extension
 
-__version__ = "1.0"
+__version__ = "1.1"
 
 tests_require = ['pytest', 'pytest-asyncio', 'mypy', 'pycodestyle']
 
