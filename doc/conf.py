2a835afa5ca86d1846413fa85dc89d075021ca9e
nikteliy <nikteliy@yandex.ru>
2023-08-15 21:47:07 +0600

    fix conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -16,7 +16,7 @@ from pathlib import Path
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, Path.resolve('..'))
+sys.path.insert(0, str(Path('..').resolve()))
 
 import snap7 # noqa: E402
 
