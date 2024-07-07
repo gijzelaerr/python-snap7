63e1599cfb779041b3e7b620b73c8c0f608b10bb
Gijs Molenaar <gijsm@spotify.com>
2024-07-07 15:32:06 +0200

    add enum to doc

--- doc/conf.py
+++ doc/conf.py
@@ -27,7 +27,7 @@ import snap7  # noqa: E402
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.coverage", "sphinx.ext.viewcode", "sphinx.ext.napoleon"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.coverage", "sphinx.ext.viewcode", "sphinx.ext.napoleon", "enum_tools.autoenum"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
